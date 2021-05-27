8d3c6c2d2a31068f68612a4597af39bd9030c4ab
Pengfei Wang <pengfei.wang@anker.com>
2021-05-27 11:14:40 +0800

    opus 使用初次提交

---
+++ app/src/main/java/com/wpf/opususetest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.wpf.opususetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Author: feipeng.wang
+ * Time:   2021/5/27
+ * Description : opus使用封装类
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/wpf/opususetest/utils/Extension.java
@@ -0,0 +1,9 @@
+package com.wpf.opususetest.utils;
+
+/**
+ * Author: feipeng.wang
+ * Time:   2021/5/27
+ * Description : 扩展类
+ */
+public class Extension {
+}
